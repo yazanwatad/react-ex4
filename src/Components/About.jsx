5c43117bcdfded96b17c4bc264abd9bd6468c602
yazanwatad <yazan-watad.14@hotmail.com>
2025-12-25 21:38:58 +0200

    added names

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,6 +6,12 @@ export default function About() {
       <h2>About </h2>
       <p>   This project is a simple React app that presents Star Wars characters and
         demonstrates routing with React Router.</p>
+         <h5>Names</h5>
+      <p>Yazan Watad : 314635863</p>
+        <p>Basel Khoury : 211641030</p>
+      <p>Eden Gavrielov : 207893363</p>
+      <p> 🌌⚔️
+ </p>
     </div>
   )
 }
